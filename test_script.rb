5223416e0c2821b7e127a1de50e0e2c7abd65d42
rajapads <rajapads@sify.com>
2014-09-02 15:22:12 +0530

    new-2sep204-15
    new-2sep204-15

--- test_script.rb
+++ test_script.rb
@@ -1,5 +1,5 @@
 print "Do you want to add or delete SSH keys [Add/Delete]: "
-File.open("/home/ubuntu/scripts/sshkeys.txt", "r") do |f|
+File.open("/home/ubuntu/scripts/sshkey.txt", "r") do |f|
   f.each_line do |line|
     puts line
   end
